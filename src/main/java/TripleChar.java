a2bf6fd99031b1c491b5a0d3d6321566ced54f57
Alexander Chadfield <chuklov@gmail.com>
2022-02-26 21:54:49 -0500

    New class

---
+++ src/main/java/TripleChar.java
@@ -0,0 +1,17 @@
+/**
+ * Find triple occurrence of a char and replace it with the number and capital letter.
+ * Example:
+ * incoming string is: aabbcccdddeeeefhhhjj
+ * outcome should be: aabb3C3D3Eej3Hjj
+ *
+ * Please write the code to make changes.
+ */
+
+
+public class TripleChar {
+
+    public String charCheck(String income) {
+
+    }
+
+}
